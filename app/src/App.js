3a67a630522a9e8cf96ca051f2879a08ec55f6fd
lai2-24 <lai2-24@itismeucci.com>
2024-05-06 10:06:37 +0200

    commit 2

--- app/src/App.js
+++ app/src/App.js
@@ -4,25 +4,28 @@ import {useEffect, useState} from 'react';
 function App() {
   const [partita, setPartita] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   const [showResult, setShowResult] = useState(false);
   const [number, setNumber] = useState();
+  const [risultato, setRisultato] = useState([]);
 
   const handleInput = (e) => {
     setNumber(e.target.value);
   };
 
-  async function invia(e){
+  async function invia(){
     const response = await fetch(`http://localhost:8080/partita/${partita.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        numero
-    }),
+        numero: number,
+      }),
     });
 
     const r = await response.json();
+    setRisultato(r);
   }
 
   async function start(){
@@ -38,18 +41,32 @@ function App() {
     const r = await response.json();
     setPartita(r);
     setIsLoading(false);
-    setShowResult(true);
+    setShowForm(true);
+  }
+
+  function result(){
+    console.log(risultato.risultato)
+    switch (risultato.risultato) {
+      case 0:
+        return <p>Hai indovinato in {risultato.tentativi}</p> 
+      case 1:
+        return <p>Troppo Grande</p>
+      case -1: 
+        return <p>Troppo Piccolo</p>
+      default:
+        return
+    }
   }
 
   return (
     <div className="App">
       <h2>Indovina Numero</h2>
       <button onClick = {start}>Nuova partita</button> <br/>
-      {showResult && 
+      {showForm && 
       <div>
         <p>
           ID: {partita.id} <br />
-          Tentativi: {partita.tentativi}
+          Tentativi: {risultato.tentativi || partita.tentativi}
         </p>
         <p>Inserisci un numero da 1 a 100
           <input type='number' onChange = {handleInput} placeholder = 'inserisci un numero'></input>
@@ -57,6 +74,7 @@ function App() {
         </p>
       </div>
       }
+      
       
 
     </div>
