a99ccffdac1bcde2286b9eaa5663b09230f030cf
lai2-24 <lai2-24@itismeucci.com>
2024-05-06 09:42:13 +0200

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -5,15 +5,29 @@ function App() {
   const [partita, setPartita] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showResult, setShowResult] = useState(false);
-  const [number, setNumber] = useState()
+  const [number, setNumber] = useState();
+
+  const handleInput = (e) => {
+    setNumber(e.target.value);
+  };
+
+  async function invia(e){
+    const response = await fetch(`http://localhost:8080/partita/${partita.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        numero
+    }),
+    });
+
+    const r = await response.json();
+  }
 
   async function start(){
     setIsLoading(true);
-
-    const handleInput = (e) => {
-      setNumber(e.target.value);
-    };
-
+    
     const response = await fetch('http://localhost:8080/partita', {
       method: 'POST',
       headers: {
@@ -24,7 +38,7 @@ function App() {
     const r = await response.json();
     setPartita(r);
     setIsLoading(false);
-    setShowResult(true)
+    setShowResult(true);
   }
 
   return (
@@ -33,10 +47,14 @@ function App() {
       <button onClick = {start}>Nuova partita</button> <br/>
       {showResult && 
       <div>
-        <p>ID: {partita.id}</p>
-        <p>Tentativi: {partita.tentativi}</p>
-
-        <input type='text' onChange = {handleInput} placeholder = 'inserisci un numero'></input>
+        <p>
+          ID: {partita.id} <br />
+          Tentativi: {partita.tentativi}
+        </p>
+        <p>Inserisci un numero da 1 a 100
+          <input type='number' onChange = {handleInput} placeholder = 'inserisci un numero'></input>
+          <button onClick = {invia}>invia</button>
+        </p>
       </div>
       }
       
